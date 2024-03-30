7bdf03c2cd38c3947b0f6765dfbee4e271c8346f
sudhakaran1502 <sudhakaran1502@gmail.com>
2024-03-31 00:55:02 +0530

    'mainproject'

---
+++ Main project 1/main2.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scroll>20){
+            $('.navbar').addclass('sticky');
+        }else{
+            $('.navbar').removeclass('sticky');
+        }
+    })
+});
