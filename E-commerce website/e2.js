2994161aaf94ac9ac15fdf90bf50b172c0e8c4bc
sudhakaran1502 <sudhakaran1502@gmail.com>
2024-04-29 22:10:10 +0530

    ecomerce

--- E-commerce website/e2.js
+++ E-commerce website/e2.js
@@ -1,2 +1,9 @@
 const wrapper=document.querySelector('.sliderwrapper'),
 menuitems=document.querySelectorAll('.menuitem');
+
+menuitems.forEach((item,index)=>{
+    item.addEventListener('click', ()=>{
+        wrapper.style.transform= `translateX(${-100*index}vw)`;
+    });
+    
+});
