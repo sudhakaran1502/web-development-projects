a0db5d2b14d26973e33432de7118823270d28c95
sudhakaran1502 <sudhakaran1502@gmail.com>
2024-05-06 22:46:41 +0530

    ecomerce

--- E-commerce website/e2.js
+++ E-commerce website/e2.js
@@ -1,9 +1,117 @@
 const wrapper=document.querySelector('.sliderwrapper'),
 menuitems=document.querySelectorAll('.menuitem');
 
+const products = [
+    {
+        id:1,
+        title:"Air Force",
+        price:119,
+        colors:[
+            {
+                code:'sandel',
+                img:'./img/air1.png',
+            },
+            {
+                code:'black',
+                img:'./img/air2.png',
+            },
+        ],
+
+    },
+    {
+        id:2,
+        title:"JORDAN",
+        price:156,
+        colors:[
+            {
+                code:'red',
+                img:'./img/jordan.png',
+            },
+            {
+                code:'black',
+                img:'./img/jordan2.png',
+            },
+        ],
+
+    },
+    {
+        id:3,
+        title:"COURT VISION",
+        price:512,
+        colors:[
+            {
+                code:'black',
+                img:'./img/courtvision.png',
+            },
+            {
+                code:'blue',
+                img:'./img/courtvision2.png',
+            },
+        ],
+
+    },
+    {
+        id:4,
+        title:"CRATER",
+        price:300,
+        colors:[
+            {
+                code:'black',
+                img:'./img/crater.png',
+            },
+            {
+                code:'white',
+                img:'./img/crater2.png',
+            },
+        ],
+
+    },
+    {
+        id:5,
+        title:"HIPPE",
+        price:350,
+        colors:[
+            {
+                code:'grey',
+                img:'./img/hippe.png',
+            },
+            {
+                code:'black',
+                img:'./img/hippe2.png',
+            },
+        ],
+
+    },
+    
+];
+
+let choosenproduct = products[0]
+
+const currentproductimg = document.querySelector('.productimg');
+const currentproducttitle = document.querySelector('.producttitle');
+const currentproductprice = document.querySelector('.productprice');
+const currentproductcolors = document.querySelectorAll('.color');
+const currentproductsizes = document.querySelectorAll('.size');
+
+
+
 menuitems.forEach((item,index)=>{
     item.addEventListener('click', ()=>{
+        //change the current slide
         wrapper.style.transform= `translateX(${-100*index}vw)`;
+
+        //change the choosen product
+        choosenproduct=products[index];
+
+        //change texts of currentproduct
+        currentproducttitle.textContent = choosenproduct.title;
+        currentproductprice.textContent = choosenproduct.price;
+        currentproductimg.src = choosenproduct.colors[0].img;
+
+        //assign new colors
+        currentproductcolors.forEach((color,index) => {
+            color.style.backgroundColor = choosenproduct.colors[index].code;
+        });
     });
     
 });
