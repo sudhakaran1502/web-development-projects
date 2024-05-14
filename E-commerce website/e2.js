a961802a90ea4cc4907df460fd12efc824c6c7a2
sudhakaran1502 <sudhakaran1502@gmail.com>
2024-05-14 23:51:46 +0530

    ecomerce

--- E-commerce website/e2.js
+++ E-commerce website/e2.js
@@ -133,3 +133,14 @@ currentproductsizes.forEach((size,index)=>{
         size.style.color='white'
     });
 });
+
+const productbutton= document.querySelector('.productbutton');
+const payment= document.querySelector('.payment');
+const close= document.querySelector('.close');
+
+productbutton.addEventListener('click',() =>{
+    payment.style.display='flex'
+})
+close.addEventListener('click',() =>{
+    payment.style.display='none'
+})
