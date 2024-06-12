f63163c0855b719d90a34457b2057f6a28e308d7
sudhakaran1502 <sudhakaran1502@gmail.com>
2024-06-12 22:52:15 +0530

    snakegame

--- snake game/s2.js
+++ snake game/s2.js
@@ -1,1 +1,21 @@
 //board
+var blocksize=25;
+var rows=20;
+var cols=20;
+var board;
+var context;
+
+window.onload = function(){
+    board = document.getElementById('board');
+    board.height = rows*blocksize;
+    board.width = cols*blocksize;
+    context = board.getContext('2d') //for drawing on the board
+
+    update();
+
+}
+function update(){
+    context.fillStyle = 'block';
+    context.fillRect(0,0,board.width, board.height);
+
+}
